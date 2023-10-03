aa1ddc02526e91583f7bae771521067107b9921e
devanshswaroop <94702422+devanshswaroop@users.noreply.github.com>
2023-10-03 17:05:41 +0530

    Add files via upload

---
+++ CustomerNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.cms.exception;
+
+
+public class CustomerNotFoundException extends RuntimeException{
+    public CustomerNotFoundException(String message){
+        super(message);
+    }
+
+}
---
+++ MyNewFilter.java
@@ -0,0 +1,13 @@
+package com.example.cms.filter;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class MyNewFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("The new filter is called");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
